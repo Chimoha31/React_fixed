c33343a7d2f722875e9e2f92e1456924329b2c6a
Chimoha31 <mokochii1108@gmail.com>
2021-10-06 12:46:54 -0700

    Use filter to extract a specific year to show

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,6 +13,10 @@ const Expenses = (props) => {
     // console.log(selectedYear);
   }
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -21,7 +25,7 @@ const Expenses = (props) => {
       onChangeFilter={filterChangeHandler}
       />
 
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
         <ExpenseItem 
         key={expense.id}
         title = {expense.title} 
@@ -30,27 +34,6 @@ const Expenses = (props) => {
         />
         ))}
 
-          {/* 上記のようにすることによってdynamicになる。動的になる。 */}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        // ></ExpenseItem> */}
       </Card>
     </div>
   );
